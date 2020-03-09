3becbab21a0c781e4a14563cb0088456d44f8b04
drohiro <rodrigorfsfr3@gmail.com>
2020-03-09 17:20:20 -0300

    Simplefy project

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
-const express = require ('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-
-app.set('view engine', 'ejs');
-
-app.use(express.static(__dirname + '/public'))
-app.listen(port, function() {
-    console.log(`Server in port: ${port}`);
-})
-
-app.get('/', (req,res) => {
-    res.render('pages/home')
-})
-
-module.exports = app;
+function converte() {
+    console.log('Função aplcou');
+    var campo_binario = document.getElementById('binarie')
+    var binario = campo_binario.value;
+    if (binario === '') {
+        
+    };
+}
